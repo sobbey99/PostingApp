322e55723415772e82f96db9b06aee55dfd68fd0
sobbey99 <sobbey99@gmail.com>
2023-02-18 16:32:52 +0200

    Signing through google and receive session

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ export default function RootLayout({children}) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
         <Nav />
         {children}</body>
     </html>
