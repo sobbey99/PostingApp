b671a2065b5945d399af92b78a82c80c2797d8a6
sobbey99 <sobbey99@gmail.com>
2023-02-19 11:35:44 +0200

    Fetching posts from DB and rendering on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 'use client'
+import axios from 'axios'
 import AddPost from './components/AddPost'
+import { useQuery } from '@tanstack/react-query'
+import Post from './components/Post'
+
+//Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get('/api/posts/getPosts');
+  return response.data
+}
 
 
 export default function Home() {
+  const {data, error, isLoading} = useQuery({
+    queryFn: allPosts,
+    queryKey: ['posts']
+  })
+  if(error) return error
+  if(isLoading) return 'Loading...'
   return (
     <main>
-      <h1>Hello next</h1>
       <AddPost />
+      {data?.map((post) => (
+        <Post 
+          key={post.id} 
+          name={post.user.name} 
+          avatar={post.user.image} 
+          postTitle={post.title}
+          id={post.id}/>
+      ))}
     </main>
   )
 }
